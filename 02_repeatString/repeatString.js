c6c3bebae90f3675696b3e5bb8fcb00a259ee13c
rajaghulamghous-0 <rajaghulamghous853@gmail.com>
2025-01-09 12:55:56 +0500

    completed the first three exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,22 @@
-const repeatString = function() {
-
+const repeatString = function (userInput, numberOfTimes) {
+  let result = "";
+  for (let i = 0; i < numberOfTimes; i++) {
+    result += userInput;
+  }
+  if (numberOfTimes < 0) {
+    return "ERROR";
+  }
+  return result;
 };
+repeatString("hey", 3);
+repeatString("hello", 10);
+repeatString("hi", 1);
+repeatString("bye", 0);
+repeatString("goodbye", -1);
+repeatString("", 10);
+let number;
+Math.random(number);
+repeatString("odin", number);
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (userInput) {
+  let myArray = userInput.split("").reverse().join("");
+  return myArray;
 };
-
+reverseString("hello");
+reverseString("hello there");
+reverseString("123! abc! Hello, Odinite.");
+reverseString("");
 // Do not edit below this line
 module.exports = reverseString;
