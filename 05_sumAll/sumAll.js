cddaf5000d4312d879533b4b86155cc645710ad3
rajaghulamghous-0 <rajaghulamghous853@gmail.com>
2025-01-09 18:58:05 +0500

    completed the 5th exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,33 @@
-const sumAll = function() {
+function sumAll(positiveIntegerOne, positiveIntegerTwo) {
+  if (
+    isNaN(positiveIntegerOne) ||
+    isNaN(positiveIntegerTwo) ||
+    positiveIntegerOne < 0 ||
+    positiveIntegerTwo < 0 ||
+    positiveIntegerOne % 1 != 0 ||
+    positiveIntegerTwo % 1 != 0 ||
+    typeof positiveIntegerOne != "number" ||
+    typeof positiveIntegerTwo != "number"
+  ) {
+    return "ERROR";
+  } else if (positiveIntegerTwo > positiveIntegerOne) {
+    let sum = 0;
+    for (let i = positiveIntegerOne; i <= positiveIntegerTwo; i++) {
+      sum += i;
+    }
+    let result = sum;
+    return parseInt(result);
+  } else if (positiveIntegerOne > positiveIntegerTwo) {
+    let sum = 0;
 
-};
+    for (let i = positiveIntegerTwo; i <= positiveIntegerOne; i++) {
+      sum += i;
+    }
+    let result = sum;
+    return parseInt(result);
+  }
+}
+parseInt(sumAll(1, 4));
 
 // Do not edit below this line
 module.exports = sumAll;
